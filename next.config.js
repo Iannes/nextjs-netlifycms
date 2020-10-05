8311f40e05cc3c16bae63ddfb99f86cf06ad9f7b
@yannis_spyrou <yannis.spyrou@vistaprint.com>
2020-10-05 18:09:03 +0200

    remove commented out code

--- next.config.js
+++ next.config.js
@@ -19,19 +19,6 @@ module.exports = {
       '/posts': { page: '/posts' }
     };
 
-    /* 
-    await fs.readdir('./_content/posts', (err, files) => {
-      files.forEach(file => {
-        const fileSlug = file.replace('.md', '');
-        console.log(`/post/${fileSlug}`);
-        paths[`/post/${fileSlug}`] = {
-          page: '/post/[slug]',
-          query: { slug: fileSlug }
-        };
-      });
-    });
-    */
-
     const files = await fs.readdir('./_content/posts');
     files.forEach(file => {
       const fileSlug = file.replace('.md', '');
